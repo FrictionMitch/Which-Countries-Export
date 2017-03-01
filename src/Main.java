3fcc6073553c3baf55ec883cd7128f035cd1e3a3
Mike Brisson <brisson22@cogeco.ca>
2017-03-01 06:33:07 -0500

    Sync

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        WhichCountriesExport export = new WhichCountriesExport();
+        export.whoExportsCoffee();
+    }
+}
---
+++ src/WhichCountriesExport.java
@@ -0,0 +1,30 @@
+import edu.duke.*;
+import org.apache.commons.csv.*;
+
+public class WhichCountriesExport {
+
+    public void listExporters(CSVParser parser, String exportOfInterest) {
+        // For each row in the csv record...
+        for (CSVRecord record : parser) {
+
+            // Look at the "Exports" column...
+            String export = record.get("Exports");
+
+            // Check to see if the record contains "export of interest"...
+            if (export.contains(exportOfInterest)) {
+
+                // If so, print the country of that row...
+                String country = record.get("Country");
+                System.out.println(country);
+            }
+
+        }
+    }
+
+    public void whoExportsCoffee() {
+//        FileResource fr = new FileResource();
+        FileResource fr = new FileResource("exportdata.csv");
+        CSVParser parser = fr.getCSVParser();
+        listExporters(parser, "coffee");
+    }
+}
